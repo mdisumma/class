3e8c9712a432cb15a69873418c52da48470a503a
Marco Di Summa <mdisumma@yahoo.com>
2021-03-18 13:11:17 +0000

    start- heuristics implementations

---
+++ heuristics.md
@@ -0,0 +1,32 @@
+1. Visibilty of system status: 
+    How well the product convays and keeps the user informed about what is (currently)going on.
+
+2.  Match between system and the real world:
+    Use words and phrases that are appropriate for the user and ones they are likely to understand(users familiar terminology). 
+
+3.  User Control and Freedom:
+    Give users a clear way back when they explore your website(back, undo and cancel options).
+
+4.  Consistency and Standards:
+     Follow platform and industry conventions. Users experience with other products set their expectations. 
+
+5.  Error Prevention
+    Design needs to try to prevent as many errors as possible, an example: present users with a confirmation option before they commit to an action.
+
+6.  Recognition
+    User should not have to remember information. You must try to reduce cognitive effort.
+
+7.  Flexability and Efficiency
+    Shortcuts may help experienced users interact with your product more effieciently.    
+    
+8.  Aesthetic designs
+   Anything displayed on your product should be relevant, otherwise it competes for attention with the relevant infromation.
+    
+17. Error recovery
+    To help users understand what errors have been made, do so in words and not via codes. Also, make the error message noticable and recognisable.
+
+18. Help and Documentation
+    May need some documentation to help users understand how to complete tasks. 
+
+source:
+https://www.nngroup.com/articles/ten-usability-heuristics/
--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 var DateTime = luxon.DateTime;
+
+
+//date method
+//https://www.w3schools.com/js/js_date_formats.asp
+var todayDate = new Date();
+console.log(`today date from method: ${todayDate}`)
+
 //GET CALL
 const getData = async () => {
     let { data } = await axios.get('https://api.coronavirus.data.gov.uk/v1/data?filters=areaType=nation;areaName=england&structure={"date":"date","newCases":"newCasesByPublishDate"}')
     return data
 }
+
+const actualDate = document.querySelector('#current-date');
+actualDate.innerHTML = DateTime.now("MMM dd y");
 
 
 //PARCE DATA
@@ -12,8 +22,8 @@ document.addEventListener('DOMContentLoaded', async () => {
     console.log(results);
 
     //LABEL
-    const casesResult = results.data[0].newCases
-    const dateResult = results.data[0].date
+    // const casesResult = results.data[0].newCases
+    // const dateResult = results.data[0].date
     // console.log(`data from luxton:${DateTime.fromISO(dateResult).toFormat('DD')}`);
 
 
@@ -21,8 +31,9 @@ document.addEventListener('DOMContentLoaded', async () => {
         const day = DateTime.fromISO(value.date).toFormat('d')
         const months = DateTime.fromISO(value.date).toFormat('LLLL')
         const year = DateTime.fromISO(value.date).toFormat('y')
-
+        // const humanData= DateTime.fromISO(value.date).toFormat('DDD')
         const printHTML = document.querySelector('#coronadata');
+        // time format (sorted from newest to old)
 
         printHTML.innerHTML += `
         <ul>
@@ -36,15 +47,22 @@ document.addEventListener('DOMContentLoaded', async () => {
         event.preventDefault()
         const dateInput = document.querySelector('#search').value;
         console.log(`date input: ${dateInput}`);
+        //recognises the input from the user and stores the value to then retrieve the requested data
+
         //human writable sequenze
-        const inputDay = dateInput.substring(0, 2)
-        console.log(inputDay)
+        const inputDay = dateInput.substring(0, 2);
+        console.log(`inputDay: ${inputDay}`)
         const inputMonth = dateInput.substring(3, 5)
-        console.log(inputMonth)
+        console.log(`inputMonth: ${inputMonth}`)
         const inputYear = dateInput.substring(6, 10)
-        console.log(inputYear)
+        console.log(`inputYear: ${inputYear}`)
         console.log(`date format: ${inputYear}-${inputMonth}-${inputDay}`);
         const isoData = `${inputYear}-${inputMonth}-${inputDay}`
+        //substring allow us to extract the characters of a string between 2 indices (0,2)-(3,5) (6,10) and return it as a new sub string.
+        //the reason for this is to format the dates in a recognisable manner (dd/mm/yyyy) instead of (yyyy/mm/dd)
+        //this follows the heuristic rules as it is recognisible to the users
+
+
         for (i = 0; i < results.data.length; i++) {
             if (results.data[i].date == isoData) {
                 const caseResult = results.data[i].newCases
